75e9dfa1bbeb0a785cd3531b5e7833d61bed21b9
Felipe Azevedo <felipeazevedo@MacBook-Pro-de-Felipe-8.local>
2023-08-14 19:51:14 -0300

    codigo da aula do dia 14/08

--- exercicios-sala/arrays.c
+++ exercicios-sala/arrays.c
@@ -1,3 +1,15 @@
 //
 // Created by Felipe Azevedo on 14/08/23.
 //
+
+#include <stdio.h>
+
+int main () {
+    int numeros[5] = {1, 2, 3, 4, 5};
+
+    char nome[10] = "Exemplo";
+
+    printf("Terceiro elemento do vetor: %d\n", numeros[2]);
+    printf("Primeira letra do nome: %c\n", nome[0]);
+    return 0;
+}
---
+++ exercicios-sala/matrizes.c
@@ -0,0 +1,5 @@
+//
+// Created by Felipe Azevedo on 14/08/23.
+//
+
+
